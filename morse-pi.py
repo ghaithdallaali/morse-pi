f38512abdaab079502cb0d795f7db5d0cc6fd955
gdalla-ali <ghaith.hadi@gmail.com>
2017-03-05 06:01:50 +0000

    Adding a working Morse code translator

--- morse-pi.py
+++ morse-pi.py
@@ -7,7 +7,8 @@ import sys
 GPIO.setmode(GPIO.BOARD)
 
 #define the pin that goes to the circuit
-pin_to_circuit = 7
+pin_to_circuit = 7 # pin ldr is connected to
+trigger = 500 # min threshhold before we record a dot/dash
 
 def ldr_reading (pin_to_circuit):
     count = 0
@@ -26,46 +27,75 @@ def ldr_reading (pin_to_circuit):
 
     return count
 
+def ldr_readings (pin_to_circuit, num_readings):
+    readings = []
+    for x in xrange(0,num_readings):
+        readings.append(ldr_reading(pin_to_circuit))
+    
+    # print "readings is %s" %readings ;# GHAITH DEBUG
+    return readings
+
+def morse_translator (morse_code):
+    CODE = {'.-'    : 'A',  '-...' : 'B' ,    '-.-.'    : 'C' ,
+            '-..'   : 'D',  '.'    : 'E' ,    '..-.'    : 'F' ,
+            '--.'   : 'G',  '....' : 'H' ,    '..'      : 'I' ,
+            '.---'  : 'J',  '-.-'  : 'K' ,    '.-..'    : 'L' ,
+            '--'    : 'M',  '-.'   : 'N' ,    '---'     : 'O' ,
+            '.--.'  : 'P',  '--.-' : 'Q' ,    '.-.'     : 'R' ,
+            '...'   : 'S',  '-'    : 'T' ,    '..-'     : 'U' ,
+            '...-'  : 'V',  '.--'  : 'W' ,    '-..-'    : 'X' ,
+            '-.--'  : 'Y',  '--..' : 'Z' ,
+            
+            # '0': '-----',  '1': '.----',  '2': '..---',
+            # '3': '...--',  '4': '....-',  '5': '.....',
+            # '6': '-....',  '7': '--...',  '8': '---..',
+            # '9': '----.' 
+        }
+
+    for char in morse_code.split('/'):
+        if char in CODE:
+            print CODE[char.upper()],
 #Catch when script is interrupted, cleanup correctly
 try:
     # Main loop
-    print "Turn off the lights, Kathie!"
-    print "Now shine the light for 5 seconds on the LDR to calibrate it..."
-    time.sleep(5)
-    if (ldr_reading(pin_to_circuit)) < 500:
-        print "Calibrated!"
-        time.sleep(1.5)
-    else:
-        print "Calibration failed :("
-        sys.exit()
+    ambient_light = sum(ldr_readings(pin_to_circuit, 5))/5
+    print "Calibrated! Ambient light is at %d" %ambient_light
+
+    # print "Turn off the lights, Kathie!"
+    # print "Now shine the light for 5 seconds on the LDR to calibrate it..."
+    # time.sleep(5)
+    # if (ldr_reading(pin_to_circuit)) < trigger:
+    #     print "Calibrated!"
+    #     time.sleep(1.5)
+    # else:
+    #     print "Calibration failed :("
+    #     sys.exit()
 
     morse_code = ""
     while True:
-        if (ldr_reading(pin_to_circuit) < 500): 
-            readings = [ldr_reading(pin_to_circuit), \
-                        ldr_reading(pin_to_circuit), \
-                        ldr_reading(pin_to_circuit), \
-                        ldr_reading(pin_to_circuit)]
+        if (ldr_reading(pin_to_circuit) < trigger): 
+            readings = ldr_readings(pin_to_circuit,4)
             for x in xrange(0,4):
-                if (readings[x] < 500):
+                if (readings[x] < trigger):
                     readings[x] = 1
                 else:
                     readings[x] = 0
             if sum(readings) >= 3:
-                morse_code+="_"
-                sys.stdout.write('_')
+                morse_code+="-"
+                sys.stdout.write('-')
                 time.sleep(0.50)
             elif 1<= sum(readings) <= 2 :
                 morse_code+="."
                 sys.stdout.write('.')
                 time.sleep(0.50)
-        elif (ldr_reading(pin_to_circuit) > 200000) :
+        elif (ldr_reading(pin_to_circuit) >= (ambient_light + 50)) :
                 morse_code+="/"
                 sys.stdout.write('/')
-                time.sleep(0.05)
+                time.sleep(0.70)
         sys.stdout.flush()
 except KeyboardInterrupt:
-    print morse_code
+    print "\n"
+    morse_translator(morse_code)
     pass
 finally:
     GPIO.cleanup()
